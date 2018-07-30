47c4ad49357bedc76cbdbc28b65f62516c2b8f07
Igor1101 <Igor1101@github.com>
2018-07-30 16:43:58 +0300

    Added Maclaurin series

--- main.java
+++ main.java
@@ -4,6 +4,7 @@ class main
 {
         public static void main(String args[])
         {
+                /* Verify logic */
                 MathUtil.verify_all();
                 Teylor ver = new series.Teylor();
                 ver.verify_all();
---
+++ series/Maclaurin.java
@@ -0,0 +1,12 @@
+package series;
+public class Maclaurin extends Teylor
+{
+        public Maclaurin(int der_number)
+        {
+                super(der_number, 0);
+        }
+        public Maclaurin()
+        {
+                super();
+        }
+}
